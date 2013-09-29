7ef8de30588b585b262066edf81d3844abb0a2d2
Joachim <joachim@nolten.org>
2013-09-29 12:53:03 +0200

    BookFetcher works.

---
+++ lib/book_fetcher.rb
@@ -0,0 +1,15 @@
+require "net/http"
+require "uri"
+
+class BookFetcher
+  def initialize
+    @http = ::Net::HTTP.new("www.googleapis.com", 443)
+    @http.use_ssl = true
+    @http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+  end
+
+  def get
+    response = @http.request(Net::HTTP::Get.new("/books/v1/volumes\?q\='ruby+on+rails'"))
+    JSON.parse response.body
+  end
+end
