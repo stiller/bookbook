62b75bca71d2d7104c163dc77deb30d26ab7fd84
Joachim <joachim@nolten.org>
2013-09-29 14:21:09 +0200

    Can create book from hash.

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,15 @@
 class Book < ActiveRecord::Base
+  def self.find_or_create_from_hash(book_hash)
+    if book = find_by_google_id(book_hash['id'])
+      return book
+    end
+
+    hash = {}
+    hash['google_id'] = book_hash['id']
+    info = book_hash['volumeInfo']
+    hash['title'] = info['title']
+    hash['authors'] = info['authors'].join(', ')
+    hash['thumbnail_url'] = book_hash['volumeInfo']['imageLinks']['smallThumbnail']
+    create(hash)
+  end
 end
--- test/models/book_test.rb
+++ test/models/book_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class BookTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Can create book from hash." do
+    book_hash = YAML::load_file(File.join(File.dirname(File.expand_path(__FILE__)),'book_hash.yml'))
+    book = Book.find_or_create_from_hash(book_hash)
+    assert_equal book.authors, 'Steve Holzner, Ph.D.'
+    assert_equal book.title, 'Beginning Ruby on Rails'
+    assert_equal book.thumbnail_url, 'http://bks9.books.google.nl/books?id=BaWayIR0CvkC&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api'
+  end
 end
